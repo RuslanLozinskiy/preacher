8c13ea5f4e300ff15101be107a21c05e08a2e425
RuslanLozinskiy <ruslanlozinskiy0@gmail.com>
2022-02-16 14:15:39 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { MainPage } from "./components/mainPage/mainPage";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MainPage />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
