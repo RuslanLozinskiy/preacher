85121b6bcd9fa9833b6e9bf2c5561fc113210245
RuslanLozinskiy <ruslanlozinskiy0@gmail.com>
2022-02-19 12:53:03 +0200

    add logo and favicon

--- src/components/mainPage/header/header.js
+++ src/components/mainPage/header/header.js
@@ -1,4 +1,9 @@
 import style from "./header.module.css";
+import { Logo } from "./logo/logo";
 export const Header = () => {
-  return <div className={style.header}></div>;
+  return (
+    <div className={style.header}>
+      <Logo />
+    </div>
+  );
 };
---
+++ src/components/mainPage/header/logo/logo.js
@@ -0,0 +1,4 @@
+import style from "./logo.module.css";
+export const Logo = () => {
+  return <img src="src\assets\logo\logo.png" alt="" />;
+};
